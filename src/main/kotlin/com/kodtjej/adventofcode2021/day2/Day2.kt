bd15fa5ede7c71f65500e7a0fd2d61f1203c8854
kodtjej <kodtjej@pm.me>
2021-12-02 22:36:23 +0100

    day 2 part 2

--- src/main/kotlin/com/kodtjej/adventofcode2021/day2/Day2.kt
+++ src/main/kotlin/com/kodtjej/adventofcode2021/day2/Day2.kt
@@ -5,38 +5,56 @@ import com.kodtjej.adventofcode2021.day1.readFile
 fun main() {
     val input = readFile("src/main/kotlin/com/kodtjej/adventofcode2021/day2/input")
 
-    val resultPart1:Int = getFinalPosition(input);
-    val resultPart2:Int = getFinalPositionWithAim(input);
+    val resultPart1:Int = getFinalPosition(input)
+    val resultPart2:Int = getFinalPositionWithAim(input)
 
-    println("Part 1: $resultPart1");
-    println("Part 2: $resultPart2");
+    println("Part 1: $resultPart1")
+    println("Part 2: $resultPart2")
 
 }
 
 fun getFinalPositionWithAim(input: List<String>): Int {
+    var horizontalPosition = 0
+    var depth = 0
+    var aim = 0
 
-    return 0;
+    for (s in input){
+        val splitString:List<String> = s.split(" ")
+        val action:String = splitString[0]
+        val amount:Int = splitString[1].toInt()
+
+        if ("forward" == action){
+            horizontalPosition += amount
+            depth += aim * amount
+        }
+        if("down" == action){
+            aim += amount
+        }
+        if("up" == action){
+            aim -= amount
+        }
+    }
+    return horizontalPosition * depth
 }
 
 fun getFinalPosition(input:List<String>):Int {
-    var horizontalPosition:Int = 0;
-    var depth:Int = 0;
+    var horizontalPosition = 0
+    var depth = 0
 
     for (s in input){
-        val splitString:List<String> = s.split(" ");
-        val action:String = splitString[0];
-        val amount:Int = splitString[1].toInt();
+        val splitString:List<String> = s.split(" ")
+        val action:String = splitString[0]
+        val amount:Int = splitString[1].toInt()
 
         if ("forward" == action){
-            horizontalPosition += amount;
+            horizontalPosition += amount
         }
         if("down" == action){
-            depth += amount;
+            depth += amount
         }
         if("up" == action){
-            depth -= amount;
+            depth -= amount
         }
     }
-    println("$horizontalPosition $depth")
-    return horizontalPosition * depth;
+    return horizontalPosition * depth
 }
