50024d73fa27e7f484070c268f559d13c06e1b51
kodtjej <kodtjej@pm.me>
2021-12-04 16:52:31 +0100

    day 3 pt 2

--- src/main/kotlin/com/kodtjej/adventofcode2021/day3/Day3.kt
+++ src/main/kotlin/com/kodtjej/adventofcode2021/day3/Day3.kt
@@ -8,9 +8,70 @@ fun main() {
     val epsilonRatePt1 = calculateRate(input, ::epsilonPredicate)
     val powerConsumption = gammaRatePt1 * epsilonRatePt1
 
-    println("Part 1 gammaRate: $gammaRatePt1")
-    println("Part 1 epsilonRate: $epsilonRatePt1")
-    println("Part 1 power consumption: $powerConsumption")
+    println("----- Part 1 -----")
+    println("gammaRate: $gammaRatePt1")
+    println("epsilonRate: $epsilonRatePt1")
+    println("power consumption: $powerConsumption")
+
+    val oxygenGeneratorRating:Int= getOxygenGeneratorRating(input)
+    val CO2ScrubberRating:Int= getCO2ScrubberRating(input)
+    val lifeSupportRating:Int = oxygenGeneratorRating * CO2ScrubberRating
+
+    println("----- Part 2 -----")
+    println("Oxygen generator rating: $oxygenGeneratorRating")
+    println("CO2 Scubber rating: $CO2ScrubberRating")
+    println("Life support rating: $lifeSupportRating")
+
+}
+
+fun getCO2ScrubberRating(input: List<String>):Int {
+    val result = getValueBasedOnLeastCommon(input, 0)
+    return  Integer.parseInt(result[0], 2)
+}
+
+fun getOxygenGeneratorRating(input: List<String>):Int{
+    val result = getValueBasedOnMostCommon(input, 0)
+    return  Integer.parseInt(result[0], 2)
+}
+
+fun getValueBasedOnLeastCommon(list: List<String>, index:Int): List<String>{
+    if(list.size == 1)
+        return list
+
+    val flippedInput = makeColumnsIntoRows(list)
+
+    val a = getLeastCommon(flippedInput, index)
+    val newList = list.filter { value -> a == value[index].digitToInt() }
+    return getValueBasedOnLeastCommon(newList, index+1)
+
+}
+
+fun getValueBasedOnMostCommon(list: List<String>, index:Int): List<String>{
+    if(list.size == 1)
+        return list
+
+    val flippedInput = makeColumnsIntoRows(list)
+
+    val a = getMostCommon(flippedInput, index)
+    val newList = list.filter { value -> a == value[index].digitToInt() }
+    return getValueBasedOnMostCommon(newList, index+1)
+
+}
+
+fun getMostCommon(input: List<String>, index:Int):Int{
+    val count = input[index].groupingBy { it }.eachCount()
+    if (count['0']!! > count['1']!!) {
+        return 0
+    }
+    return 1
+}
+
+fun getLeastCommon(input: List<String>, index:Int):Int{
+    val count = input[index].groupingBy { it }.eachCount()
+    if (count['0']!! > count['1']!!) {
+        return 1
+    }
+    return 0
 }
 
 fun gammaPredicate(zeros:Int, ones:Int):Boolean {
@@ -38,7 +99,6 @@ fun calculateRate(input: List<String>, predFn: (val1:Int, val2:Int) -> Boolean):
 
 fun makeColumnsIntoRows(input: List<String>): List<String> {
     val flippedValues = mutableListOf<String>()
-
     for (column in 0 until input[0].length) {
         var newRow = ""
         for (row in input) {
@@ -46,6 +106,5 @@ fun makeColumnsIntoRows(input: List<String>): List<String> {
         }
         flippedValues.add(newRow)
     }
-
     return flippedValues
 }
