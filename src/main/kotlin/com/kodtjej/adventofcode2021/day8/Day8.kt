a167da8939b896d9f893f5784243729c374d7eda
kodtjej <kodtjej@gmail.com>
2021-12-14 14:33:35 +0100

    day 8 pt 1

--- src/main/kotlin/com/kodtjej/adventofcode2021/day8/Day8.kt
+++ src/main/kotlin/com/kodtjej/adventofcode2021/day8/Day8.kt
@@ -5,4 +5,25 @@ import com.kodtjej.adventofcode2021.day1.readFile
 fun main() {
     val input = readFile("src/main/kotlin/com/kodtjej/adventofcode2021/day8/input")
 
+    val outputValues = getOutputValues(input).map { it.split(" ").filter { it != "" } }
+    val res = outputValues.flatMap { it.map { decideOnNumber(it) } }.filter { it != -1 }
+    println(res.size)
+}
+
+
+fun getOutputValues(input: List<String>): List<String> {
+    val outputRegex = Regex("^.+\\|(.*)\$")
+    return input.map { outputRegex.find(it)?.groupValues?.get(1) ?: "" }
+}
+
+fun decideOnNumber(input: String): Int {
+
+    when (input.length) {
+        2 -> return 1
+        3 -> return 7
+        4 -> return 4
+        7 -> return 8
+    }
+
+    return -1
 }
