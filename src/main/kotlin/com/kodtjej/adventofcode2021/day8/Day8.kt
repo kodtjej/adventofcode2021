a6e13bf249860fa6329b37134f9687525c17abfa
kodtjej <kodtjej@gmail.com>
2021-12-14 14:36:39 +0100

    refactoring

--- src/main/kotlin/com/kodtjej/adventofcode2021/day8/Day8.kt
+++ src/main/kotlin/com/kodtjej/adventofcode2021/day8/Day8.kt
@@ -6,8 +6,9 @@ fun main() {
     val input = readFile("src/main/kotlin/com/kodtjej/adventofcode2021/day8/input")
 
     val outputValues = getOutputValues(input).map { it.split(" ").filter { it != "" } }
-    val res = outputValues.flatMap { it.map { decideOnNumber(it) } }.filter { it != -1 }
-    println(res.size)
+    val appearances = outputValues.flatMap { it.map { decideOnNumber(it) } }.filter { it != -1 }
+    println("---- Part 1 ----")
+    println("Times 1, 4, 7, 8 appears: ${appearances.size}")
 }
 
 
@@ -17,13 +18,11 @@ fun getOutputValues(input: List<String>): List<String> {
 }
 
 fun decideOnNumber(input: String): Int {
-
     when (input.length) {
         2 -> return 1
         3 -> return 7
         4 -> return 4
         7 -> return 8
     }
-
     return -1
 }
